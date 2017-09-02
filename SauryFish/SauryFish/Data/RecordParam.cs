1802fd78e144b572a3152826a7156c102916d4c2
730530507 <730530507@qq.com>
2017-09-02 15:02:51 +0800

    1

---
+++ SauryFish/SauryFish/Data/RecordParam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SauryFish.Data
+{
+    public class RecordParam
+    {
+        public string Search { set; get; }
+        public int PageIndex { set; get; }
+        public int PageSize { set; get; }
+    }
+}
---
+++ SauryFish/SauryFish/Dto/RecordDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SauryFish.Dto
+{
+    public class RecordDto
+    {
+        public int EnrollNumber { set; get; }
+        public string Name { set; get; }
+        public DateTime AttendancedOn { set; get; }
+    }
+}
